c3f48f525c15c96441ceddf4a0b42f2fc3ccdb98
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2021-05-06 12:14:19 +0200

    email

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="http://rekog.purplematrixhosting.co.uk/wp-content/uploads/2021/04/rekonio_logo.png" class="logo" alt="Rekonio Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
